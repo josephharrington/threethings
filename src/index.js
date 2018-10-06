74ca47a7804c97c965596d9c1fce2a05ff4880fc
Joey Harrington <jharrington@netflix.com>
2018-10-06 02:58:40 -0700

    0.5

--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ import * as dat from 'dat.gui';
 
 window.THREE = THREE;
 require('three/examples/js/controls/OrbitControls');
+require('three/examples/js/exporters/STLExporter');
 
 
+let mesh, 
+    wireframe, 
+    scene, 
+    renderer, 
+    camera, 
+    exporter,
+    ground,
+    lights,
+    controls
+    ;
 
+// defaults
 const params = {
     resolution: 20,
-    bigR: 300,
-    lilR: 125,
-    lilP: 90,
-    extrusionSegments: 1000,
-    radiusSegments: 16,
-    radius: 20,
+    bigR: 252,
+    lilR: 231,
+    lilP: 60,
+    // ell: 0.26,  // lilp/lilr = 60/231
+    // kay: 0.912,  // lilR/bigR = 231/252
+    extrusionSegments: 500,
+    radiusSegments: 8,
+    radius: 10,
     closed: false,
-    showPts: true,
+    showPts: false,
+    oscMagnitude: 50,
+    oscSpeed: 0.2,
+    oscillations: 3,
+
+    enableFog: true,
+    enableShadows: false,
+    showWireframe: true,
+
+    saveStl: saveStl,
+
 };
 
 const vals = {
@@ -24,25 +48,36 @@ const vals = {
     dt: -1,
 };
 
-var mesh, spheres, wireframe, scene, renderer, camera;
-
 function initGui() {
+    exporter = new THREE.STLExporter();
+
     const gui = new dat.GUI();
-    gui.add(params, 'resolution', 1, 50).step(1).onChange(updateMath);
+    gui.add(params, 'resolution', 1, 100).step(1).onChange(updateMath);
     gui.add(params, 'bigR', 10, 1000).step(1).onChange(updateMath);
     gui.add(params, 'lilR', 10, 1000).step(1).onChange(updateMath);
-
     gui.add(params, 'lilP', 10, 1000).step(1).onChange(updateModel);
-    gui.add(params, 'extrusionSegments', 5, 2000).step(5).onChange(updateModel);
+
+    gui.add(params, 'extrusionSegments', 5, 10000).step(5).onChange(updateModel);
     gui.add(params, 'radiusSegments', 1, 32).step(1).onChange(updateModel);
     gui.add(params, 'radius', 1, 100).step(1).onChange(updateModel);
     gui.add(params, 'closed').onChange(updateModel);
     gui.add(params, 'showPts').onChange(updateModel);
+
+    gui.add(params, 'oscMagnitude', -200, 200).onChange(updateModel);
+    gui.add(params, 'oscSpeed', 0, Math.PI / 2).onChange(updateModel);
+    gui.add(params, 'oscillations', 0, 30).step(1).onChange(updateModel);
+
+    gui.add(params, 'enableFog').onChange(updateScene);
+    gui.add(params, 'enableShadows').onChange(updateScene);
+    gui.add(params, 'showWireframe').onChange(updateModel);
+
+    gui.add(params, 'saveStl');
+
     gui.open();
 }
 
 function updateMath() {
-    const { bigR, lilR, resolution } = params;
+    const { bigR, lilR, ell, kay, resolution } = params;
     const lcm = calcLcm(bigR, lilR);
     vals.numRots = lcm / bigR;
     vals.numPts = lcm / lilR;
@@ -57,19 +92,13 @@ function updateModel() {
         scene.remove(mesh);
         mesh.geometry.dispose();
     }
-    if (spheres !== undefined) {
-        spheres.forEach(sphere => {
-            scene.remove(sphere);
-            sphere.geometry.dispose();
-        });
-    }
 
     const { bigR, lilR, lilP } = params;
     const points = [];
-    for (let t = 0.0; t + vals.dt/2 <= 2 * Math.PI * vals.numRots; t += vals.dt) {
+    for (let t = 0.0; t - vals.dt <= 2 * Math.PI * vals.numRots; t += vals.dt) {
         const x = (bigR - lilR) * Math.cos(t) + lilP * Math.cos(t * (bigR - lilR) / lilR);
         const y = (bigR - lilR) * Math.sin(t) - lilP * Math.sin(t * (bigR - lilR) / lilR);
-        points.push(new THREE.Vector3(x, y, 50 * Math.cos( vals.numPts * t / (4 * vals.numRots))));
+        points.push(new THREE.Vector3(x, y, params.oscMagnitude * Math.cos( t * params.oscillations / vals.numRots )));
     }
 
     const path = new THREE.CurvePath();
@@ -83,45 +112,60 @@ function updateModel() {
         color: 0x156289, emissive: 0x072534, side: THREE.DoubleSide, flatShading: true });
     var dotMaterial = new THREE.MeshLambertMaterial({ 
         color: 0xff00ff, transparent: true, opacity: 1 });
-    
-    spheres = [];
+
+    const tubeGeometry = new THREE.TubeBufferGeometry( 
+        path, params.extrusionSegments, params.radius, params.radiusSegments, params.closed);
+    mesh = new THREE.Mesh(tubeGeometry, meshMaterial);
+    mesh.position.set(0, 200, 0);
+    if (params.showWireframe) {
+        wireframe = new THREE.LineSegments(tubeGeometry, lineMaterial);
+        mesh.add(wireframe);
+    }
+    scene.add(mesh);
+
+    // points
     if (params.showPts) {
         for (let i = 0; i < points.length; i++) {
             const sphere = new THREE.SphereGeometry(params.radius * 1.2, 8, 8);
             sphere.translate(points[i].x, points[i].y, points[i].z);
             const sphereMesh = new THREE.Mesh(sphere, dotMaterial);
-            spheres.push(sphereMesh);
-            scene.add(sphereMesh);
+            mesh.add(sphereMesh);
         }
     }
+}
 
-    const tubeGeometry = new THREE.TubeBufferGeometry( 
-        path, params.extrusionSegments, params.radius, params.radiusSegments, params.closed);
-    mesh = new THREE.Mesh(tubeGeometry, meshMaterial);
-    wireframe = new THREE.LineSegments(tubeGeometry, lineMaterial);
-    mesh.add(wireframe);
-    scene.add(mesh);
+function updateScene() {
+    // fog
+    scene.fog = params.enableFog ? new THREE.FogExp2( 0xa0a0a0, 0.001 ) : null;
+
+    // shadow
+    renderer.shadowMap.enabled = params.enableShadows;
+    ground.receiveShadow = params.enableShadows;
+    lights.forEach(light => light.castShadow = params.enableShadows);
+    mesh && (mesh.castShadow = params.enableShadows);
 }
 
 function init() {
     // camera
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 10000);
-    camera.position.set(0, 50, 500);
+    camera.position.set(0, 300, 300);
+    camera.lookAt(0, 200, 0);
 
     // scene
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xf0f0f0);
+    scene.background = new THREE.Color( 0xa0a0a0 );
 
     // light
-    [
+    lights = [
         [0, 200, 0], 
         [100, 200, 100], 
         [-100, -200, -100],
 
-    ].forEach(([x, y, z]) => {
+    ].map(([x, y, z]) => {
         const light = new THREE.PointLight(0xffffff, 1, 0);
         light.position.set(x, y, z);
         scene.add(light);
+        return light;
     });
 
     // renderer
@@ -131,12 +175,45 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     // controls
-    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.target = new THREE.Vector3(0, 200, 0);
+    // controls.autoRotate = true;
+    // controls.autoRotateSpeed = 0.5;
+    controls.update();
+
+    // permanent objects
+    ground = new THREE.Mesh( 
+        new THREE.PlaneBufferGeometry( 10000, 10000 ), 
+        new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+    ground.rotation.x = - Math.PI / 2;
+    scene.add( ground );
+
+    updateScene();
 }
 
 function animate() {
     requestAnimationFrame(animate);
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
+    // controls.update();
+}
+
+function saveStl() {
+    const result = exporter.parse(mesh);
+    saveString(result, 'box.stl');
+}
+
+function saveString(text, filename) {
+    save(new Blob([text], {type: 'text/plain'}), filename);
+}
+
+const link = document.createElement('a');
+link.style.display = 'none';
+document.body.appendChild(link);
+
+function save( blob, filename ) {
+    link.href = URL.createObjectURL( blob );
+    link.download = filename;
+    link.click();
 }
 
 function calcGcd(a, b) {
